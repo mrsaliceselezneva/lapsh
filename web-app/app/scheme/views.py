2d49ea04701acfc513b34aa549b693b6dc936468
IronArt <poluvesov.artyom@gmail.com>
2022-05-31 19:45:54 +0400

    find xml

--- web-app/app/scheme/views.py
+++ web-app/app/scheme/views.py
@@ -28,13 +28,15 @@ class NewsUpdateView(UpdateView):
     model = Mierda
     template_name = 'scheme/update.html'
     fields = ['title', 'body']
-    context_object_name = 'therms'
+    context_object_name = 'therm'
 
 
 def relationships_terms(request):
     therm = Therm.objects.all()
     connection = Connection.objects.all()
-    xml_file = XmlFile.objects.get(id=len(XmlFile.objects.all()))
+    v = XmlFile.objects.all()[-1]
+    v = v.id
+    xml_file = XmlFile.objects.get(id=v)
 
     s = ''
     for line in open(xml_file.file.url[1:], 'r'):
@@ -47,8 +49,6 @@ def relationships_terms(request):
             v[key] = jsn[key]
 
     jsn = v
-    # jsn = json.dumps(v)
-
     answers = []
     sl_therm = {}
     sl_connector = {}
@@ -100,30 +100,54 @@ def relationships_terms(request):
         if el == 'Shape':
             if not isinstance(jsn[el], list):
                 name = jsn[el]['mxCell']['@style']
-            else:
-                name = jsn[el][0]['mxCell']['@style']
-        else:
-            name = el
-        if name in therm:
-            if not isinstance(jsn[el], list):
-                sl_therm[jsn[el]['@id']] = jsn[el]['mxCell']['@parent']
-                true_therm[jsn[el]['@id']] = name
-                all_el.append([jsn[el]['@id'], jsn[el]['@label']])
+                if name in therm:
+                    if not isinstance(jsn[el], list):
+                        sl_therm[jsn[el]['@id']] = jsn[el]['mxCell']['@parent']
+                        true_therm[jsn[el]['@id']] = name
+                        all_el.append([jsn[el]['@id'], jsn[el]['@label']])
+                    else:
+                        for a in jsn[el]:
+                            sl_therm[a['@id']] = a['mxCell']['@parent']
+                            true_therm[a['@id']] = name
+                            all_el.append([a['@id'], a['@label']])
+                else:
+                    if not isinstance(jsn[el], list):
+                        sl_connector[jsn[el]['@id']] = [jsn[el]['mxCell']['@source'], jsn[el]['mxCell']['@target']]
+                        true_connector[jsn[el]['@id']] = name
+                    else:
+                        for a in jsn[el]:
+                            sl_connector[a['@id']] = [a['mxCell']['@source'], a['mxCell']['@target']]
+                            true_connector[a['@id']] = name
             else:
                 for a in jsn[el]:
+                    name = a['mxCell']['@style']
+
                     sl_therm[a['@id']] = a['mxCell']['@parent']
                     true_therm[a['@id']] = name
                     all_el.append([a['@id'], a['@label']])
-        else:
-            if not isinstance(jsn[el], list):
-                sl_connector[jsn[el]['@id']] = [jsn[el]['mxCell']['@source'], jsn[el]['mxCell']['@target']]
-                true_connector[jsn[el]['@id']] = name
-            else:
-                for a in jsn[el]:
-                    sl_connector[a['@id']] = [a['mxCell']['@source'], a['mxCell']['@target']]
-                    true_connector[a['@id']] = name
-    id = 1
 
+        else:
+            name = el
+            if name in therm:
+                if not isinstance(jsn[el], list):
+                    sl_therm[jsn[el]['@id']] = jsn[el]['mxCell']['@parent']
+                    true_therm[jsn[el]['@id']] = name
+                    all_el.append([jsn[el]['@id'], jsn[el]['@label']])
+                else:
+                    for a in jsn[el]:
+                        sl_therm[a['@id']] = a['mxCell']['@parent']
+                        true_therm[a['@id']] = name
+                        all_el.append([a['@id'], a['@label']])
+            else:
+                if not isinstance(jsn[el], list):
+                    sl_connector[jsn[el]['@id']] = [jsn[el]['mxCell']['@source'], jsn[el]['mxCell']['@target']]
+                    true_connector[jsn[el]['@id']] = name
+                else:
+                    for a in jsn[el]:
+                        sl_connector[a['@id']] = [a['mxCell']['@source'], a['mxCell']['@target']]
+                        true_connector[a['@id']] = name
+
+    id = 1
     for i in sl_therm:
         if int(sl_therm[i]) > 1:
             answers.append({
@@ -151,20 +175,24 @@ def relationships_terms(request):
                     'connection': connection['adjacent']
                 })
                 id += 1
-
     for c in sl_connector:
+        # if if if
+        con = connection['Connector']
+        if true_therm[sl_connector[c][0]] == 'Rect' and true_therm[sl_connector[c][1]] == 'cylinder':
+            con = 'istochnick'
+
         answers.append({
             'id': str(id),
             'therm1': therm[true_therm[sl_connector[c][0]]],
             'therm2': therm[true_therm[sl_connector[c][1]]],
-            'connection': connection['Connector']
+            'connection': con
         })
         id += 1
         answers.append({
             'id': str(id),
             'therm1': therm[true_therm[sl_connector[c][1]]],
             'therm2': therm[true_therm[sl_connector[c][0]]],
-            'connection': connection['Connectorfrom']
+            'connection': con
         })
         id += 1
 
@@ -222,7 +250,7 @@ def createConnection(request: HttpRequest):
             form.save()
             return redirect('./')
         else:
-            error = 'неверный формат данных'
+            error = 'Неверный формат данных'
     form = ConnectionForm()
     data = {
         'form': form,
@@ -303,32 +331,59 @@ def addXML(request: HttpRequest):
                                 sl_connector[a['@id']] = [a['mxCell']['@source'], a['mxCell']['@target']]
                                 true_connector[a['@id']] = name
                 all_el = []
+
                 for el in jsn:
                     if el == 'Shape':
                         if not isinstance(jsn[el], list):
                             name = jsn[el]['mxCell']['@style']
-                        else:
-                            name = jsn[el][0]['mxCell']['@style']
-                    else:
-                        name = el
-                    if name in therm:
-                        if not isinstance(jsn[el], list):
-                            sl_therm[jsn[el]['@id']] = jsn[el]['mxCell']['@parent']
-                            true_therm[jsn[el]['@id']] = name
-                            all_el.append([jsn[el]['@id'], jsn[el]['@label']])
+                            if name in therm:
+                                if not isinstance(jsn[el], list):
+                                    sl_therm[jsn[el]['@id']] = jsn[el]['mxCell']['@parent']
+                                    true_therm[jsn[el]['@id']] = name
+                                    all_el.append([jsn[el]['@id'], jsn[el]['@label']])
+                                else:
+                                    for a in jsn[el]:
+                                        sl_therm[a['@id']] = a['mxCell']['@parent']
+                                        true_therm[a['@id']] = name
+                                        all_el.append([a['@id'], a['@label']])
+                            else:
+                                if not isinstance(jsn[el], list):
+                                    sl_connector[jsn[el]['@id']] = [jsn[el]['mxCell']['@source'],
+                                                                    jsn[el]['mxCell']['@target']]
+                                    true_connector[jsn[el]['@id']] = name
+                                else:
+                                    for a in jsn[el]:
+                                        sl_connector[a['@id']] = [a['mxCell']['@source'], a['mxCell']['@target']]
+                                        true_connector[a['@id']] = name
                         else:
                             for a in jsn[el]:
+                                name = a['mxCell']['@style']
+
                                 sl_therm[a['@id']] = a['mxCell']['@parent']
                                 true_therm[a['@id']] = name
                                 all_el.append([a['@id'], a['@label']])
+
                     else:
-                        if not isinstance(jsn[el], list):
-                            sl_connector[jsn[el]['@id']] = [jsn[el]['mxCell']['@source'], jsn[el]['mxCell']['@target']]
-                            true_connector[jsn[el]['@id']] = name
+                        name = el
+                        if name in therm:
+                            if not isinstance(jsn[el], list):
+                                sl_therm[jsn[el]['@id']] = jsn[el]['mxCell']['@parent']
+                                true_therm[jsn[el]['@id']] = name
+                                all_el.append([jsn[el]['@id'], jsn[el]['@label']])
+                            else:
+                                for a in jsn[el]:
+                                    sl_therm[a['@id']] = a['mxCell']['@parent']
+                                    true_therm[a['@id']] = name
+                                    all_el.append([a['@id'], a['@label']])
                         else:
-                            for a in jsn[el]:
-                                sl_connector[a['@id']] = [a['mxCell']['@source'], a['mxCell']['@target']]
-                                true_connector[a['@id']] = name
+                            if not isinstance(jsn[el], list):
+                                sl_connector[jsn[el]['@id']] = [jsn[el]['mxCell']['@source'],
+                                                                jsn[el]['mxCell']['@target']]
+                                true_connector[jsn[el]['@id']] = name
+                            else:
+                                for a in jsn[el]:
+                                    sl_connector[a['@id']] = [a['mxCell']['@source'], a['mxCell']['@target']]
+                                    true_connector[a['@id']] = name
                 id = 1
 
                 for i in sl_therm:
@@ -360,18 +415,23 @@ def addXML(request: HttpRequest):
                             id += 1
 
                 for c in sl_connector:
+                    # if if if
+                    con = connection['Connector']
+                    if true_therm[sl_connector[c][0]] == 'Rect' and true_therm[sl_connector[c][1]] == 'cylinder':
+                        con = 'istochnick'
+
                     answers.append({
                         'id': str(id),
                         'therm1': therm[true_therm[sl_connector[c][0]]],
                         'therm2': therm[true_therm[sl_connector[c][1]]],
-                        'connection': connection['Connector']
+                        'connection': con
                     })
                     id += 1
                     answers.append({
                         'id': str(id),
                         'therm1': therm[true_therm[sl_connector[c][1]]],
                         'therm2': therm[true_therm[sl_connector[c][0]]],
-                        'connection': connection['Connectorfrom']
+                        'connection': con
                     })
                     id += 1
 
@@ -399,9 +459,6 @@ def addXML(request: HttpRequest):
                     )
                     cnt += 1
 
-            # kekeekekekekeeke
-
-
             therm = Therm.objects.all()
             connection = Connection.objects.all()
             xml_file = XmlFile.objects.get(id=len(XmlFile.objects.all()))
@@ -417,7 +474,6 @@ def addXML(request: HttpRequest):
                     v[key] = jsn[key]
 
             jsn = v
-            # jsn = json.dumps(v)
 
             answers = []
             sl_therm = {}
@@ -439,7 +495,6 @@ def addXML(request: HttpRequest):
                 obr_connection[str(el.id)] = str(el.mark)
             connection = v
 
-            true_therm = {}
             true_connector = {}
             all_el = []
             for el in jsn:
@@ -453,12 +508,10 @@ def addXML(request: HttpRequest):
                 if name in therm:
                     if not isinstance(jsn[el], list):
                         sl_therm[jsn[el]['@id']] = jsn[el]['mxCell']['@parent']
-                        true_therm[jsn[el]['@id']] = name
                         all_el.append([jsn[el]['@id'], jsn[el]['@label']])
                     else:
                         for a in jsn[el]:
                             sl_therm[a['@id']] = a['mxCell']['@parent']
-                            true_therm[a['@id']] = name
                             all_el.append([a['@id'], a['@label']])
                 else:
                     if not isinstance(jsn[el], list):
@@ -476,14 +529,14 @@ def addXML(request: HttpRequest):
                         'id': str(id),
                         'therm1': sl_therm[i],
                         'therm2': i,
-                        'connection': connection['parent']
+                        'connection': obr_connection[connection['parent']]
                     })
                     id += 1
                     answers.append({
                         'id': str(id),
                         'therm1': i,
                         'therm2': sl_therm[i],
-                        'connection': connection['child']
+                        'connection': obr_connection[connection['child']]
                     })
                     id += 1
 
@@ -494,47 +547,53 @@ def addXML(request: HttpRequest):
                             'id': str(id),
                             'therm1': i,
                             'therm2': j,
-                            'connection': connection['adjacent']
+                            'connection': obr_connection[connection['adjacent']]
                         })
                         id += 1
 
+            therm = {}
+            for el in true_therm:
+                therm[true_therm[el]] = el
+            print(sl_connector)
             for c in sl_connector:
+                # if if if
+                con = obr_connection[connection['Connector']]
+                if true_therm[sl_connector[c][0]] == 'Rect' and true_therm[sl_connector[c][1]] == 'cylinder':
+                    con = 'istochnick'
+                if true_therm[sl_connector[c][0]] == 'Rect' and true_therm[sl_connector[c][1]] == 'rhombus':
+                    con = 'time'
+                print(sl_connector[c][0], sl_connector[c][1])
                 answers.append({
                     'id': str(id),
                     'therm1': sl_connector[c][0],
                     'therm2': sl_connector[c][1],
-                    'connection': connection['Connector']
+                    'connection': con
                 })
                 id += 1
                 answers.append({
                     'id': str(id),
                     'therm1': sl_connector[c][1],
                     'therm2': sl_connector[c][0],
-                    'connection': connection['Connectorfrom']
+                    'connection': con
                 })
                 id += 1
 
-            cnt = 1
-            answers2 = []
-            v = []
-            lol = {}
-
-
-            v = {}
-            for el in true_therm:
-                v[true_therm[el]] = el
-            true_therm = v
-            v = {}
-            for el in therm:
-                v[therm[el]] = el
-            therm = v
-            print(true_therm)
-            print(therm)
-            print(answers)
-            print(all_el)
+            for el in answers:
+                v1 = ''
+                for i in all_el:
+                    if i[0] == el['therm1']:
+                        v1 = i[1]
+                if v1 == 'Reserve':
+                    print(el)
+            connection = Connection.objects.all()
             for el in answers:
                 v1 = ''
                 v2 = ''
+                con = ''
+                for i in connection:
+                    if i.mark == el['connection']:
+                        con = i.title
+                        break
                 for i in all_el:
                     if i[0] == el['therm1']:
                         v1 = i[1]
@@ -545,11 +604,11 @@ def addXML(request: HttpRequest):
                     title=v1,
                     web='/scheme/' + v1,
                     therm_title=v2,
-                    connection=obr_connection[el['connection']]
+                    connection=con
                 )
             return redirect('./relationships_terms')
         else:
-            error = 'неверный формат данных'
+            error = 'Неверный формат данных'
     form = XMLForm()
     data = {
         'form': form,
